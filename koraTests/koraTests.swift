3e9e746085c521608144732d4842413c09f42b72
Jose Sanchez <engineer@copydataai.com>
2026-06-22 08:48:59 +0100

    Initial Commit

---
+++ koraTests/koraTests.swift
@@ -0,0 +1,16 @@
+//
+//  koraTests.swift
+//  koraTests
+//
+//  Created by Jose Sanchez on 22/06/2026.
+//
+
+import Testing
+
+struct koraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
